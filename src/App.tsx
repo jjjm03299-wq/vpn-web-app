be1a1fdaee1ef79cb8e0cb61c0b288dc017120c7
jjjm03299-wq <jjjm03299@gmail.com>
2026-09-14 07:52:28 +0300

    Create index.html, src/App.tsx, and src/main.tsx entry points

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RouterProvider } from '@tanstack/react-router';
+import router from './router/router';
+import './App.css';
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
